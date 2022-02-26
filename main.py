15a7021ac766dd6a9cc6e44e8fccd4c091bead70
Yu Zhao <yuzhao@zhaoyudeMacBook-Pro.local>
2022-02-26 09:40:43 +0800

    第二次提交

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# This is a sample Python script.
+# This is a sample.
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
